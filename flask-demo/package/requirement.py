ee2980b2f6058cfec3768bdf2285bd4a24f185c1
liuziping <787696331@qq.com>
2015-11-26 20:12:18 +0800

    Update requirement.py
    fix

--- flask-demo/package/requirement.py
+++ flask-demo/package/requirement.py
@@ -1,6 +1,6 @@
-Flask (0.10.1)
-Flask-RESTful (0.3.4)
-Flask-Script (2.0.5)
-Flask-SQLAlchemy (2.0)
-MySQL-python (1.2.5)
+Flask
+Flask-RESTful
+Flask-Script 
+Flask-SQLAlchemy 
+MySQL-python 
 requests 
