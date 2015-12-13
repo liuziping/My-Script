9b87cc3d07b8d02462583b7d14d648db41326dd9
liuziping <787696331@qq.com>
2015-12-13 21:55:08 +0800

    删除旧文件

--- token_req.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#coding:utf-8
-import requests
-
-#r = requests.get("http://127.0.0.1:5000/login",auth=('liuziping','123456'))
-#print r.text
-token = 'bGl1emlwaW5nOjAuMzM4MzM5MDM5OTY6MTQ0NjU3NDI0OC4zNQ=='
-r = requests.get("http://127.0.0.1:5000/test",params={'token':token})
-print r.text
