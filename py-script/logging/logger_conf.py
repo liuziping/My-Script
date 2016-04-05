f1a7857d2bb2d1a7fb4b1eb264a238d46fee8093
liuziping <787696331@qq.com>
2016-04-06 00:09:22 +0800

    添加logging.config自定义配置日志

---
+++ py-script/logging/logger_conf.py
@@ -0,0 +1,9 @@
+import logging
+import logging.config
+
+logging.config.fileConfig("logger.conf")
+logger = logging.getLogger("example02")
+
+logger.debug('This is debug message')
+logger.info('This is info message')
+logger.warning('This is warning message')
