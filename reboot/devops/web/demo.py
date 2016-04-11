efde5ad9649190efaac35f6fcd6fcc942e6b5d7a
liuziping <787696331@qq.com>
2016-04-11 21:41:08 +0800

    每个页面都加上session［usernname］

--- reboot/devops/web/demo.py
+++ reboot/devops/web/demo.py
@@ -18,20 +18,22 @@ def index():
 def user(htmlname):
     if session.get('author','nologin') == 'nologin':
         return redirect('/login')
-    util.write_log('web').info("info")
-    return render_template(htmlname +'.html')
+    username = session.get('username')
+    return render_template(htmlname +'.html',user=username)
 
 @app.route('/project/<htmlname>')
 def project(htmlname):
     if session.get('author','nologin') == 'nologin':
         return redirect('/login')
-    return render_template(htmlname +'.html')
+    username = session.get('username')
+    return render_template(htmlname +'.html',user=username)
 
 @app.route('/cmdb/<htmlname>')
 def cmdb(htmlname):
     if session.get('author','nologin') == 'nologin':
         return redirect('/login')
-    return render_template(htmlname +'.html')
+    username = session.get('username')
+    return render_template(htmlname +'.html',user=username)
 
 @app.errorhandler(404)    #系统自带的装饰器，遇到404回自动返回制定的404页面
 def not_found(e):
