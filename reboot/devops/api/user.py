9d0d5c4ecb750284240a1b03adf3965cf26cfc64
liuziping <787696331@qq.com>
2016-04-12 15:03:16 +0800

    用户模块编写完成

--- reboot/devops/api/user.py
+++ reboot/devops/api/user.py
@@ -8,18 +8,6 @@ import json, traceback,hashlib
 
 #本模块提供用户信息的增删改查，以及 用户所在组，所有权限的查询
 
-#将字符串转为列表 传入的参数如[u'1,2'] or [u'1,2', u'1,3,4']，结果为['1','2','3','4']
-def getid_list(ids):   
-    if not isinstance(ids, list):
-        return None
-    id_list = []
-    for x in ids:                #每次循环出x如：'1,2','2,3,4'   
-        id_list +=x.split(',')   #将每次循环输出的字符串转为列表，并将列表合并
-    id_list = set(id_list)       #将列表去重
-    #print id_list
-    return  id_list
-
-
 #创建用户
 @jsonrpc.method('user.create')
 @auth_login
@@ -50,9 +38,10 @@ def createuser(auth_info,*arg,**kwargs):
         util.write_log('api').info(username, "create_user %s" % data['username'])
         return json.dumps({'code': 0, 'result': '创建用户%s成功' % data['username']})
     except:
-        util.write_log('api').error("Create user error: %s" % traceback.format_exc())
+         util.write_log('api').error("Create user error: %s" % traceback.format_exc())
         return json.dumps({'code':  1, 'errmsg': '创建用户失败，有异常情况'})
-#获取(uid or username)个人信息，个人信息展示和个人修改信息，以及用户列表中更新用户信息部分的获取数据
+
+#通过传入的条件，通常为id，查询某条用户的信息，用于管理员修改用户信息
 @jsonrpc.method('user.get')
 @auth_login
 def userinfo(auth_info,**kwargs):
@@ -62,7 +51,9 @@ def userinfo(auth_info,**kwargs):
     try:
         output = ['id','username','name','email','mobile','role','is_lock','r_id']
         fields = kwargs.get('output',output) #api可以指定输出字段，如果没有指定output，就按默认output输出
-        where = kwargs.get('where',None)     #前端传来的where条件，可能是uid或者username
+        where = kwargs.get('where',None)     #前端传来的where条件
+        if not where:
+            return json.dumps({'code':1, 'errmsg':'传一个where条件'})
         result = app.config['cursor'].get_one_result('user', fields, where)
         if result is '':
             return json.dumps({'code':1, 'errmsg':'必须选择一个用户'})
@@ -73,7 +64,7 @@ def userinfo(auth_info,**kwargs):
         return json.dumps({'code':1,'errmsg':'获取用户信息失败'})
 
 '''
-#获取用户信息
+#获取用户具体的信息，包括基本信息，所属组，所拥有的权限，用于用户个人中心的展示和个人资料更新
 @jsonrpc.method('user.getinfo')
 @auth_login
 def userselfinfo(auth_info,**kwargs):
@@ -85,26 +76,26 @@ def userselfinfo(auth_info,**kwargs):
         user = app.config['cursor'].get_one_result('user', fields, where={'username': username})
         if user.get('r_id', None):
             r_id = user['r_id'].split(',')
-
             #获取组所有的id,name并存为字典如：{'1': 'sa', '2': 'php'}
-            gids = app.config['cursor'].get_results('user_group', ['id', 'name', 'p_id'], where={'id': r_id})
+            rids = app.config['cursor'].get_results('role', ['id', 'name', 'p_id'], where={'id': r_id})
         else:
-            gids = {}
-        own_pids = set([])
-        for x in gids:
-                own_pids |= set(x['p_id'].split(','))
-        user['r_id'] = [x['name'] for x in gids]
+            rids = {}
+        pids = []
+        for x in rids:    
+                pids += x['p_id'].split(',')   
+        pids=list(set(pids))   #去重。通过用户名查到其角色id，在通过角色id取到用户的权限id
+        user['r_id'] = [x['name'] for x in rids]   #将角色id转为角色名
 
-        if own_pids:
-            pids = app.config['cursor'].get_results('permission', ['id', 'name', 'name_cn', 'url'], where={'id': list(own_pids)})
-            user['p_id'] = dict([(str(x['name']), dict([(k, x[k]) for k in ('name_cn', 'url')])) for x in pids])
+        if pids:   #将用户的权限id转为权限名
+            mypids = app.config['cursor'].get_results('power', ['id', 'name', 'name_cn', 'url'], where={'id': pids})
+            user['p_id'] = dict([(str(x['name']), dict([(k, x[k]) for k in ('name_cn', 'url')])) for x in mypids])
         else:
             user['p_id'] = {}
 
-        util.write_log(username, 'get_user_info')
-        return  json.dumps({'co de': 0, 'user': user})
+        util.write_log('api').info(username, 'get_user_info')
+        return  json.dumps({'code': 0, 'user': user})
     except:
-        logging.getLogger().error("Get users list error: %s" % traceback.format_exc())
+        logging.getLogger('api').error("Get users list error: %s" % traceback.format_exc())
         return json.dumps({'code':1,'errmsg':'获取用户信息失败'})
 
 #获取用户列表
@@ -120,18 +111,19 @@ def userlist(auth_info,**kwargs):
     try:
         if '1' not in r_id:
             return json.dumps({'code': 1,'errmsg':'只有管理员才有此权限' })
-        #获取组所有的id,name并存为字典如：{'1': 'sa', '2': 'php'}
-        #gids = app.config['cursor'].get_results('user_group', ['id', 'name'])
-        #gids = dict([(str(x['id']), x['name']) for x in gids])
+        #获取角色的id,name并存为字典如：{'1': 'sa', '2': 'php'}
+        rids = app.config['cursor'].get_results('role', ['id', 'name'])
+        rids = dict([(str(x['id']), x['name']) for x in rids])
+
         result = app.config['cursor'].get_results('user', fields)
-        for user in result: #查询user表中的r_id,与user_groups表生成的字典对比，一致则将id替换为name,如，"sa,php"
-            user['r_id'] = ','.join([gids[x] for x in user['r_id'].split(',') if x in gids])
+        for user in result: #查询user表中的r_id,与role表生成的字典对比，一致则将id替换为name,如，"sa,php"
+            user['r_id'] = ','.join([rids[x] for x in user['r_id'].split(',') if x in rids])
             users.append(user)
-        util.write_log(username, 'get_all_users')
-         return  json.dumps({'code': 0, 'users': users,'count':len(users)})
+        util.write_log('api').info(username, 'get_all_users')
+        return  json.dumps({'code': 0, 'users': users,'count':len(users)})
     except:
         logging.getLogger().error("Get users list error: %s" % traceback.format_exc())
-         return json.dumps({'code':1,'errmsg':'获取用户列表失败'})
+        return json.dumps({'code':1,'errmsg':'获取用户列表失败'})
  
 #更新用户信息
 @jsonrpc.method('user.update')
@@ -140,23 +132,20 @@ def userupdate(auth_info, **kwargs):
     if auth_info['code'] == 1:
         return json.dumps(auth_info)
     username = auth_info['username']
-    if auth_info['role'] != '0':
-        return json.dumps({'code':1,'errmsg':'只有管理员才有此权限'})
     try:
         data = request.get_json()['params']
         where = data.get('where',None)
         data = data.get('data',None)
-        print data,where
-        if int(auth_info['role']) == 0:
+        if '1' in auth_info['r_id']:   #管理员更新用户信息
             result = app.config['cursor'].execute_update_sql('user', data, where)
-        else:
-            result =  app.config['cursor'].execute_update_sql('user', data, where,['name','username','email','mobile'])
+        else:                          #用户更新个人信息
+            result = app.config['cursor'].execute_update_sql('user', data, where,['name','username','email','mobile'])
         if result == '':
             return json.dumps({'code':1, 'errmsg':'需要指定一个用户'})
-        util.write_log(username, 'update groups %s success!' % data['name'])
+        util.write_log('api').info(username, 'update groups %s success!' % data['name'])
         return  json.dumps({'code':0,'result':'更新用户%s成功' % data['name']})
     except:
-        logging.getLogger().error("update error: %s"  % traceback.format_exc())
+        util.write_log('api').error("update error: %s"  % traceback.format_exc())
         return json.dumps({'code':1, 'errmsg':"更新用户失败"})
 
 
@@ -167,17 +156,17 @@ def userdelete(auth_info, **kwargs):
     if auth_info['code'] == 1:
         return json.dumps(auth_info)
     username = auth_info['username']
-    if auth_info['role'] != '0':
+    if '1' not in auth_info['r_id']:
         return json.dumps({'code':1,'errmsg':'只有管理员才有此权限'})
     try: 
-        data = request.get_json()['params']
-        result = app.config['cursor'].execute_delete_sql('user', data)
+        where = request.get_json()['params']
+        result = app.config['cursor'].execute_delete_sql('user', where)
         if result == '':
-            return json.dumps({'code':1,'errmsg':'需要指定一个用户'})
-        util.write_log(username, 'delete user successed')
+            return json.dumps({'code':1,'errmsg':'需要传入一个where条件'})
+        util.write_log('api').info(username, 'delete user successed')
         return json.dumps({'code':0,'result':'删除用户成功'})
     except:
-        logging.getLogger().error('delete groups error: %s' %  traceback.format_exc())
+        util.write_log('api').error('d elete groups error: %s' %  traceback.format_exc())
         return json.dumps({'code':1,'errmsg':'删除用户失败'})
 
 
@@ -185,20 +174,20 @@ def userdelete(auth_info, **kwargs):
 @app.route('/api/password',methods=['PUT'])
 @auth_login
 def passwd(auth_info):
-    if auth_info['code'] == 1:   #主要用于判断认证是否过期，过期会会在web提示
+    if auth_info['code'] == 1:  
         return json.dumps(auth_info)
     username = auth_info['username']
     uid = int(auth_info['uid'])
-    role = int(auth_info['role'])
+    r_id = auth_info['r_id'])
     try:
-         data = request.get_json()
-        if role==0 and 'user_id' in data:   # admin no need oldpassword  but need user_id
+        data = request.get_json()
+        if '1' in r_id and data.has_key('user_id'):   #管理员修改用户密码，需要传入user_id,不需要输入老密码
             user_id = data['user_id']
             if not app.config['cursor'].if_userid_exist(user_id): 
                 return json.dumps({'code':1,'errmsg':'需要更改密码的用户不存在'})
             password = hashlib.md5(data['password']).hexdigest()
             app.config['cursor'].execute_update_sql('user', {'password': password}, {'id': user_id})
-        else:                  #user  need input oldpassword
+        else:                  #用户自己修改密码，需要输入老密码
             if not data.has_key("oldpassword") :
                 return json.dumps({'code':1,'errmsg':'需要提供原密码'})
             oldpassword = hashlib.md5(data['oldpassword']).hexdigest()
@@ -208,58 +197,11 @@ def passwd(auth_info):
             password = hashlib.md5(data['password']).hexdigest()
             app.config['cursor'].execute_update_sql('user', {'password': password}, {'username': username})
 
-        if not git_passwd(username, data['password']):
-            return json.dumps({'code': 1, 'errmsg': 'Git密码更新失败，请联系管理员'})
-        util.write_log(username,'update user password')
+        util.write_log('api').info(username,'update user password success')
         return json.dumps({'code':0,'result':'更新密码成功'})
     except:
-        logging.getLogger().error('update user password error : %s' % traceback.format_exc())
+        util.write_log('api').error('update user password error : %s' % traceback.format_exc())
         return json.dumps({'code':1,'errmsg':'更新密码有异常'})
 
 
-#用户登录成功后通过用户username获取自己的权限信息，个人中心用户权限部分使用，点击权限名会连接到响应的url
-@jsonrpc.method('user_perm.getlist')
-@auth_login
-def getlist(auth_info,**kwargs):
-    if auth_info['code']==1:
-        return json.dumps(auth_info)
-    username = auth_info['username']
-    id = auth_info['uid']
-    try:
-        output = ['name','name_cn','url','info']
-        fields = kwargs.get('output', output)
-
-        res = app.config['cursor'].get_one_result('user', ['r_id'], {'username': username})
-        r_id = getid_list([res['r_id']])
-
-        res = app.config['cursor'].get_results('group', ['p_id'], {'id': r_id})
-        perm_result = getid_list([x['p_id'] for x in res])
-
-        result = app.config['cursor'].get_results('permission', fields, {'id': perm_result})
-        util.write_log(username, "get permission success")
-        return json.dumps({'resu lt':0,'result':result,'count':len(result)})
-    except:
-        logging.getLogger().error("get list permission error: %s"  %  traceback.format_exc())
-        return json.dumps({'cod e':1,'errmsg':'获取权限列表失败'})
-
-#用户登录成功后通过用户username获取自己的组信息，个人中心调用
-@jsonrpc.method('user_groups.getlist')
-@auth_login
-def user_groups(auth_info, **kwargs):
-    if auth_info['code'] == 1: 
-        return json,dumps(auth_info)
-    username = auth_info['username']
-    try:
-        output = ['id','name','name_cn','p_id','info']
-        fields = kwargs.get('output', output)
-
-        res = app.config['cursor'].get_one_result('user', ['r_id'], {'username': username})
-        r_id= getid_list([res['r_id']])
-
-        result = app.config['cursor'].get_results('user_group', fields, {'id': r_id})
-        util.write_log(username, "get groups success")
-        return json.dumps({'code':0,'result':result,'count':len(result)})
-    except:
-        logging.getLogger().error("get list groups error: %s"  % traceback.format_exc())
-        return json.dumps({'code':1, 'errmsg':'获取组列表失败'})
 '''
