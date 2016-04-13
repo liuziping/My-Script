b14aaebf6252af38de6f21aadfac594af9bc90be
liuziping <787696331@qq.com>
2016-04-14 00:31:39 +0800

    个人中心套页面

--- reboot/devops/web/demo.py
+++ reboot/devops/web/demo.py
@@ -11,29 +11,43 @@ headers = {'content-type': 'application/json'}
 def index():
     if session.get('author','nologin') == 'nologin':
         return redirect('/login')
-    username = session.get('username')
-    return render_template('index.html',user=username)
+    headers['authorization'] = session['author']
+    url = "http://%s/api" % app.config['api_host']
+    data = {'jsonrpc': '2.0', 'id': 1, 'method': 'user.getinfo'}
+    req = requests.post(url, headers=headers, json=data)
+    result = json.loads(json.loads(req.content).get('result', '{}'))
+    if result['code'] == 0:
+        user = result['user']
+        session['user'] = result['user']  #用户信息存入session
+        session['role'] = user['r_id'] #角色名eg:['sa','php']
+        session['perm'] = user['p_id'].keys()  #权限名eg:['git','mysql']
+        session['username'] = user['name'] if user['name'] else user['username']
+        return render_template('index.html',info=session,user=session['user'])
+    else:
+        return redirect('/login')
 
 @app.route('/user/<htmlname>')
 def user(htmlname):
     if session.get('author','nologin') == 'nologin':
         return redirect('/login')
-    username = session.get('username')
-    return render_template(htmlname+'.html',user=username)
+    user = session['user']
+    user['role'] = ','.join(user['r_id'])
+    user['perm'] = ','.join(['<a href="%s" style="color:blue">%s</a>' % (x['url'], x['name_cn']) for x in user['p_id'].values()])
+    return render_template(htmlname+'.html',info=session,user=user)
 
 @app.route('/project/<htmlname>')
 def project(htmlname):
     if session.get('author','nologin') == 'nologin':
         return redirect('/login')
     username = session.get('username')
-    return render_template(htmlname+'.html',user=username)
+    return render_template(htmlname+'.html',info=session,user=username)
 
 @app.route('/cmdb/<htmlname>')
 def cmdb(htmlname):
     if session.get('author','nologin') == 'nologin':
         return redirect('/login')
     username = session.get('username')
-    return render_template(htmlname+'.html',user=username)
+    return render_template(htmlname+'.html',info=session,user=username)
 
 @app.errorhandler(404)    #系统自带的装饰器，遇到404回自动返回制定的404页面
 def not_found(e):
