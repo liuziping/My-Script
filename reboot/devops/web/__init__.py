183a7b627085699eeb19ab37331f628d979fb8c5
liuziping <787696331@qq.com>
2016-04-15 01:48:02 +0800

    用户列表套页面

--- reboot/devops/web/__init__.py
+++ reboot/devops/web/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask                                                                                                                                      
 app = Flask(__name__)
 
-import demo,login
+import demo,public,login
 
--- reboot/devops/web/demo.py
+++ reboot/devops/web/demo.py
@@ -30,24 +30,68 @@ def index():
 def user(htmlname):
     if session.get('author','nologin') == 'nologin':
         return redirect('/login')
-    user = session['user']
-    user['role'] = ','.join(user['r_id'])
-    user['perm'] = ','.join(['<a href="%s" style="color:blue">%s</a>' % (x['url'], x['name_cn']) for x in user['p_id'].values()])
-    return render_template(htmlname+'.html',info=session,user=user)
+    headers['authorization'] = session['author']
+    validate_result = json.loads(util.validate(session['author'], app.config['passport_key']))
+    if int(validate_result['code']) == 0:
+        user = session['user']
+        user['role'] = ','.join(user['r_id'])
+        user['perm'] = ','.join(['<a href="%s" style="color:blue">%s</a>' % (x['url'], x['name_cn']) for x in user['p_id'].values()])
+        return render_template(htmlname+'.html',info=session,user=user)
+    else:
+       return render_template(htmlname+'.html',errmsg=validate_result['errmsg']) 
 
 @app.route('/project/<htmlname>')
 def project(htmlname):
     if session.get('author','nologin') == 'nologin':
         return redirect('/login')
-    username = session.get('username')
-    return render_template(htmlname+'.html',info=session,user=username)
+    headers['authorization'] = session['author']
+    validate_result = json.loads(util.validate(session['author'], app.config['passport_key']))
+    if int(validate_result['code']) == 0:
+        return render_template(htmlname+'.html',info=session,user=session['user'])
+    else:
+       return render_template(htmlname+'.html',errmsg=validate_result['errmsg']) 
 
 @app.route('/cmdb/<htmlname>')
 def cmdb(htmlname):
     if session.get('author','nologin') == 'nologin':
         return redirect('/login')
-    username = session.get('username')
-    return render_template(htmlname+'.html',info=session,user=username)
+    headers['authorization'] = session['author']
+    validate_result = json.loads(util.validate(session['author'], app.config['passport_key']))
+    if int(validate_result['code']) == 0:
+        return render_template(htmlname+'.html',info=session,user=username)
+    else:
+        return render_template(htmlname+'.html',errmsg=validate_result['errmsg']) 
+
+
+
+#管理员修改用户密码
+@app.route("/user/changepasswd",methods=['GET','POST'])
+def changepasswd():
+    if session.get('username') == None :
+        return redirect('/login')
+    headers['authorization'] = session['author']
+    if request.method == 'POST':
+        user_id = int(request.form.get('passwdid'))
+        password = request.form.get('changepasswd')
+        data = {'user_id':user_id,'password':password}
+        url = "http://%s/api/password" % app.config['api_host']
+        r = requests.post(url, headers=headers,json=data)
+        return r.text
+
+#用户修改个人密码
+@app.route("/user/chpwdoneself",methods=['GET','POST'])
+def chpwdoneself():
+    if session.get('username') == None:
+        return redirect('/login')
+    headers['authorization'] = session['author']
+    if request.method == 'POST':
+        oldpasswd = request.form.get('oldpasswd')
+        newpasswd = request.form.get('newpasswd')
+        data = {'oldpassword':oldpasswd,'password':newpasswd}
+        url = "http://%s/api/password" % app.config['api_host']
+        r = requests.post(url, headers=headers,json=data)
+        return r.text
+
 
 @app.errorhandler(404)    #系统自带的装饰器，遇到404回自动返回制定的404页面
 def not_found(e):
