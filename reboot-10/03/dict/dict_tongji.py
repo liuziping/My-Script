96bb188174226167565380a367e60ad29f6f2539
liuziping <787696331@qq.com>
2016-07-23 10:58:40 +0800

    利用dict.get方法优化代码

--- reboot-10/03/dict/dict_tongji.py
+++ reboot-10/03/dict/dict_tongji.py
@@ -5,13 +5,13 @@ content="who have touched their lives Love begins with a smile grows with a kiss
 
 result = {}
 for s in content.split(" "):
-    if s in result:
-        result[s] +=1 
+    if s in result:          
+        result[s] +=1       # result[s] = result.get(s,0)+1   get优化写法替换if else
     else:
         result[s] = 1 
 print result
 res  = {} 
 for k,v in result.items():
-    res.setdefault(v,[])           #dict.setdefault(k,v) 给字典设置默认值，对应的get,获取字典的默认值
+    res.setdefault(v,[])    # dict.setdefault(k,v) 给字典设置默认值，对应的get,获取字典的默认值
     res[v].append(k)
 print res 
