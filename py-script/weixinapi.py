47d2ad1df063f367c9819a482fad45c4a3bce749
liuziping <liuziping@yuanxin-inc.com>
2017-09-29 16:29:11 +0800

    update weixinapi.py

--- py-script/weixinapi.py
+++ py-script/weixinapi.py
@@ -3,33 +3,37 @@
 import requests
 import json
 import time 
+import sys
+
+content = sys.argv[1]
 headers = {"Content-Type": "application/json","charset": "utf-8"} 
 
 def gettoken():
-    data={'corpid':'wx43053cde0306cd1a','corpsecret':'OdUzcPVbt1fUkiXBRrQ0n00DAutfQqYZ63I606VLiIvu-UMUFrDKx9jTMgbHgqpT'}
-    tokenurl="https://qyapi.weixin.qq.com/cgi-bin/gettoken"
-    r=requests.get(tokenurl,params=data)
-    # print repr(r.content)          # 返回的是json字符串
+    data = {'corpid':'wx43053cde0306cd1a','corpsecret':'OdUzcPVbt1fUkiXBRrQ0n00DAutfQqYZ63I606VLiIvu-UMUFrDKx9jTMgbHgqpT'}
+    tokenurl = "https://qyapi.weixin.qq.com/cgi-bin/gettoken"
+    r=requests.get(tokenurl,params=data,verify=False)
+    #print repr(r.content)          # 返回的是json字符串
     token = json.loads(r.content)
     token = token['access_token']
     return token
 
 def sendmsg(msg):
-    text = {
-       "touser":"liuziping1989",
+    values = {
+       "touser":"@all",
+       # "touser":"liuziping1989",
        #"toparty":3,
        #'totag':1,
        "msgtype": "text",
        "agentid": 2,
        "text": {
-           "content": "Warning!!! %s" % msg
+           "content": "%s" % msg
        },
        "safe":"0"
     }
 
     token = gettoken() 
     texturl = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s' % token 
-    r = requests.post(texturl, headers=headers, data=str(text).replace("'",'"'))
+    r = requests.post(texturl, json.dumps(values),verify=False)
     return r.content
 
-print sendmsg("IDC is wrong")
+print sendmsg(content)
